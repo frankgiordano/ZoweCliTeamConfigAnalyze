30e1f3f6bf068ba079cf01b4feb29fd23734f0c6
Frank Giordano <giofr01@yahoo.com>
2022-08-06 00:32:47 -0400

    added comment about ProfileProperties

--- src/main/java/zowecli/globalteamconfig/sections/ProfileProperties.java
+++ src/main/java/zowecli/globalteamconfig/sections/ProfileProperties.java
@@ -2,6 +2,8 @@ package zowecli.globalteamconfig.sections;
 
 public class ProfileProperties {
 
-    // this will have many props most are optional, lets make this a builder pattern as such..
+    // this will have many props most are optional, as such lets make this a builder pattern
+    // but are the properties specify in the config json file known? if not and any value property can be
+    // added then we will have a Map<String, String> properties variable
 
 }
