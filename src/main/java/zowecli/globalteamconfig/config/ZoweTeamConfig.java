bb50e1ee5e4c52e7d9f6e8e2b9896d24ed68d348
Frank Giordano <giofr01@yahoo.com>
2022-08-05 23:26:15 -0400

    use getter and setter for now

--- src/main/java/zowecli/globalteamconfig/config/ZoweTeamConfig.java
+++ src/main/java/zowecli/globalteamconfig/config/ZoweTeamConfig.java
@@ -12,4 +12,44 @@ public class ZoweTeamConfig {
     private Defaults defaults;
     private AutoStore autoStore;
 
+    public List<Partition> getPartitions() {
+        return partitions;
+    }
+
+    public void setPartitions(List<Partition> partitions) {
+        this.partitions = partitions;
+    }
+
+    public Schema getSchema() {
+        return schema;
+    }
+
+    public void setSchema(Schema schema) {
+        this.schema = schema;
+    }
+
+    public List<Profile> getProfiles() {
+        return profiles;
+    }
+
+    public void setProfiles(List<Profile> profiles) {
+        this.profiles = profiles;
+    }
+
+    public Defaults getDefaults() {
+        return defaults;
+    }
+
+    public void setDefaults(Defaults defaults) {
+        this.defaults = defaults;
+    }
+
+    public AutoStore getAutoStore() {
+        return autoStore;
+    }
+
+    public void setAutoStore(AutoStore autoStore) {
+        this.autoStore = autoStore;
+    }
+    
 }
--- src/main/java/zowecli/globalteamconfig/sections/Partition.java
+++ src/main/java/zowecli/globalteamconfig/sections/Partition.java
@@ -7,4 +7,20 @@ public class Partition {
     private ProfileProperties profileProperties;
     private List<Profile> profiles;
 
+    public ProfileProperties getProfileProperties() {
+        return profileProperties;
+    }
+
+    public void setProfileProperties(ProfileProperties profileProperties) {
+        this.profileProperties = profileProperties;
+    }
+
+    public List<Profile> getProfiles() {
+        return profiles;
+    }
+
+    public void setProfiles(List<Profile> profiles) {
+        this.profiles = profiles;
+    }
+
 }
