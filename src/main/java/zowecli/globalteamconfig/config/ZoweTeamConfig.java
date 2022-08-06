074e7a4527c6502f3e06363c2f6e5bb023644f1b
Frank Giordano <giofr01@yahoo.com>
2022-08-06 00:46:30 -0400

    make ZoweTeamConfig immutable

--- src/main/java/zowecli/globalteamconfig/config/ZoweTeamConfig.java
+++ src/main/java/zowecli/globalteamconfig/config/ZoweTeamConfig.java
@@ -12,44 +12,33 @@ public class ZoweTeamConfig {
     private Defaults defaults;
     private AutoStore autoStore;
 
-    public List<Partition> getPartitions() {
-        return partitions;
+    public ZoweTeamConfig(List<Partition> partitions, Schema schema, List<Profile> profiles,
+                          Defaults defaults, AutoStore autoStore) {
+        this.partitions = partitions;
+        this.schema = schema;
+        this.profiles = profiles;
+        this.defaults = defaults;
+        this.autoStore = autoStore;
     }
 
-    public void setPartitions(List<Partition> partitions) {
-        this.partitions = partitions;
+    public List<Partition> getPartitions() {
+        return partitions;
     }
 
     public Schema getSchema() {
         return schema;
     }
 
-    public void setSchema(Schema schema) {
-        this.schema = schema;
-    }
-
     public List<Profile> getProfiles() {
         return profiles;
-    }
-
-    public void setProfiles(List<Profile> profiles) {
-        this.profiles = profiles;
     }
 
     public Defaults getDefaults() {
         return defaults;
     }
 
-    public void setDefaults(Defaults defaults) {
-        this.defaults = defaults;
-    }
-
     public AutoStore getAutoStore() {
         return autoStore;
-    }
-
-    public void setAutoStore(AutoStore autoStore) {
-        this.autoStore = autoStore;
     }
     
 }
