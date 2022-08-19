8ad9d322faafb75a1bbb2a7290c14353295af04e
Frank Giordano <giofr01@yahoo.com>
2022-08-19 16:19:47 -0400

    add constants

--- src/main/java/zowe/teamconfig/service/KeyTarService.java
+++ src/main/java/zowe/teamconfig/service/KeyTarService.java
@@ -9,16 +9,18 @@ import java.util.List;
 public class KeyTarService {
 
     private final List<String> serviceNames = List.of("Zowe", "Zowe-Plugin");
+    private final String ACCOUNT_NAME = "secure_config_props";
+    private final String ERROR_MSG = "No zowe configuration information available";
 
     public KeyTarConfig getKeyTar(String keyString) throws Exception {
-        KeyTarContainer keyTarContainer = new KeyTarContainer("", "secure_config_props", keyString);
+        KeyTarContainer keyTarContainer = new KeyTarContainer("", ACCOUNT_NAME, keyString);
         return keyTarContainer.getKeyConfigs().get(0);
     }
 
     public KeyTarConfig getKeyTar() throws Exception {
         List<KeyTarConfig> keyTarConfigs = new ArrayList<>();
         for (String serviceName : serviceNames) {
-            KeyTarContainer keyTarContainer = new KeyTarContainer(serviceName, "secure_config_props");
+            KeyTarContainer keyTarContainer = new KeyTarContainer(serviceName, "ACCOUNT_NAME");
             try {
                 keyTarContainer.processKey();
             } catch (Exception e) {
@@ -29,7 +31,7 @@ public class KeyTarService {
             break;
         }
         if (keyTarConfigs.isEmpty()) {
-            throw new Exception("No zowe configuration information available");
+            throw new Exception(ERROR_MSG);
         }
         return keyTarConfigs.get(0);
     }
